9e088a2e5eeac554c2e06e93f3c4e3ef61a4353d
moby <mobylanlan@gmail.com>
2026-07-03 05:54:12 +0800

    style:微調

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   // 固定使用 4321 埠；若被占用會直接報錯（不會偷偷跳到 4322 造成混淆）
@@ -9,10 +11,13 @@ export default defineConfig({
     port: 4321,
     host: false,
   },
+
   vite: {
     plugins: [tailwindcss()],
     server: {
       strictPort: true,
     },
   },
+
+  adapter: cloudflare()
 });
